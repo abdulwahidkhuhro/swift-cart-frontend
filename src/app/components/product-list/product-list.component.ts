90f43bc949033c10fa5b20a1c7533223de0f4147
Abdul Wahid <abdulwahidkhuhro1@gmail.com>
2024-04-12 16:31:58 +0500

    created product-list-component

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products: Product[] = [];
+  constructor(private productService: ProductService) { }
+
+
+  ngOnInit(): void {
+    this.listProducts();
+    console.log("Hello How are you?")
+    console.log("Size : "+this.products.length)
+    for(let i = 0; i < this.products.length; i++){
+        console.log(this.products[i].name)
+    }
+  }
+
+  listProducts(){
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+  }
+
+ 
+
+}

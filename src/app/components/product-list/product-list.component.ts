446e90835c91db9e50ca5a45839505e31fb53ef6
Abdul Wahid <abdulwahidkhuhro1@gmail.com>
2024-04-17 23:01:12 +0500

    added view for category id endpoint in backend

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,31 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  // templateUrl: './product-list-table.component.html',
-  // templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
-  constructor(private productService: ProductService) { }
+  currentCategoryId :number = 0;
+  constructor( private productService: ProductService,
+               private route: ActivatedRoute ) {
+               
+               }
 
 
   ngOnInit(): void {
-    this.listProducts();
-    console.log("Hello How are you?")
-    console.log("Size : "+this.products.length)
-    for(let i = 0; i < this.products.length; i++){
-        console.log(this.products[i].name)
-    }
+    this.route.paramMap.subscribe(() => {
+      this.listProducts()
+    });
   }
 
   listProducts(){
-    this.productService.getProductList().subscribe(
+
+    //check if id parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if(hasCategoryId){
+      //get the id param string. convet string to number using the + symbol
+      if(this.route.snapshot.paramMap.get('id') != null){
+        this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+      }else{
+        this.currentCategoryId = 1;
+      }
+    }
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
